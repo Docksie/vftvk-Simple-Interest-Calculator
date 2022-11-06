95faf6c66d958b9bcbd13fbe58584ed113f86142
Docksie <seigrist36@gmail.com>
2022-11-06 18:01:13 -0500

    Update script.js
    Fixed the amount input to require a positive integer

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ document.querySelector("#range").addEventListener("input", function(e){
 
 function compute() {
     var amount = document.getElementById("principal").value;
+    if(amount <= 0 || amount == '') {
+        window.alert("Enter a positive integer greater than 0");
+        document.getElementById("principal").focus();
+        return;
+    }
     var interestRate = document.getElementById("range").value / 100;
     var numOfYears = document.getElementById("years").value;
     var result = document.getElementById("resultDisplay").innerHTML = amount * interestRate * numOfYears;
