8f2c1b38b6f0d1fb12b574d83f5c78bc751f35d9
Docksie <seigrist36@gmail.com>
2022-11-06 16:51:41 -0500

    updated sic
    updated simple interest calculator for ibm project

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+document.querySelector("#range").addEventListener("input", function(e){
+    document.querySelector(".range").textContent=e.currentTarget.value + "%";
+}) 
+
+function compute() {
+    var amount = document.getElementById("principal").value;
+    var interestRate = document.getElementById("range").value / 100;
+    var numOfYears = document.getElementById("years").value;
+    var result = document.getElementById("resultDisplay").innerHTML = amount * interestRate * numOfYears;
+    document.getElementById("amountDisplay").innerHTML = amount;
+    document.getElementById('rangeDisplay').innerHTML = interestRate;
+    document.getElementById('resultDisplay').innerHTML = result;
+    document.getElementById('yearsDisplay').innerHTML = numOfYears;
 }
-        
